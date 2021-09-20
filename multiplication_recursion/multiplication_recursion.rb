2ef71242bcddcc85e4ad139e9aa9c476577bfebc
dolee <dollosa.christian@gmail.com>
2021-09-21 05:42:35 +0800

    completed multiplication recursion test file. added test files inside challenge folder

--- multiplication_recursion/multiplication_recursion.rb
+++ multiplication_recursion/multiplication_recursion.rb
@@ -2,9 +2,3 @@ def multiply(a,b)
   multiply(b,a) if a < b
   b.zero? ? 0 : a + multiply(a,b-1)
 end
-
-puts "#{multiply(2,3)}"
-puts "#{multiply(3,3)}"
-puts "#{multiply(8,5)}"
-puts "#{multiply(4,6)}"
-puts "#{multiply(10,6)}"
--- spec/spec_helper.rb
+++
@@ -1,2 +0,0 @@
-require_relative '../index_of_alphabet/*'
-require_relative '../multiplication_recursion/*
