05578ad8710af66bed2fb8fed39a326661ab3f89
dolee <dollosa.christian@gmail.com>
2021-09-21 05:43:58 +0800

    fixed multiplication by recursion test file

--- multiplication_recursion/multiplication_recursion_spec.rb
+++ multiplication_recursion/multiplication_recursion_spec.rb
@@ -1,8 +1,8 @@
 require_relative './multiplication_recursion.rb'
 
-describe 'Index of Alphabet' do
-  it "should return 6" do expect(multiplication_recursion(2,3)).to eq(6) end
-  it "should return 9" do expect(multiplication_recursion(3,3)).to eq(9) end
-  it "should return 40" do expect(multiplication_recursion(8,5)).to eq(40) end
-  it "should return 60" do expect(multiplication_recursion(6,10)).to eq(60) end
+describe 'Multiplication Recursion' do
+  it "should return 6" do expect(multiply(2,3)).to eq(6) end
+  it "should return 9" do expect(multiply(3,3)).to eq(9) end
+  it "should return 40" do expect(multiply(8,5)).to eq(40) end
+  it "should return 60" do expect(multiply(6,10)).to eq(60) end
 end
