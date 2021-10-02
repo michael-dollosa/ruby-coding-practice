e0b27754817c825194827cae30b13bd8e24e6307
Michael Dollosa <michaeldollosa@dolees-Macbook-Pro.local>
2021-10-02 13:10:13 +0800

    modified logic

--- one_away/one_away.rb
+++ one_away/one_away.rb
@@ -10,5 +10,8 @@
 def one_away(original, edited)
   #general logic for getting differences
   diff = original.chars - edited.chars
+  # additional checking in for special cases where two strings results to 0 just because they have similar characters but are different words
+  # example would be "pale", "plae" - this would result to 0 (no change) based on logic, but is actually expected to be false since it needs more than 1 procedure
+  return false if diff.empty?
   diff.count < 2 ? true : false
 end
